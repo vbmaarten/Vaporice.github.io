ae040bbf1b0e32f7e4d602f93476574845d52cdb
Maarten van Beek <maartenvanbeek@gmail.com>
2014-07-31 17:40:52 +0200

    mob

--- index.js
+++ index.js
@@ -19,11 +19,14 @@ function imgsize(){
 	if(aswin < aspect){
 		$("#koffer").css("width", String($(window).width())+"px");
 		$("#koffer").css("height", String($(window).width()/aspect)+"px");		
-		$("#koffer").css("margin-top", ($(window).height-$("#koffer").height())/2);
+		$("#koffer").css("margin-top", String(($(window).height-$("#koffer").height())/2)+"px");
+		$("#koffer").css("margin-left", "0px");
 	} else {		
 		$("#koffer").css("height", String($(window).height())+"px");
 		$("#koffer").css("width", String($(window).height()*aspect)+"px");
-		$("#koffer").css("margin-top", ($(window).height-$("#koffer").height())/2);
+		$("#koffer").css("margin-left", String(($(window).height-$("#koffer").height())/2)+"px");
+		$("#koffer").css("margin-top", "0px");
+
 	}
 }
 
